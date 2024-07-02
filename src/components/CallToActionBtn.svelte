b32298e890e223fd6e80a31ba906c60ac1d89665
davidtheblane <davidtheblane@gmail.com>
2024-07-02 09:03:53 -0300

    include links in callToActionBtn and Footer

--- src/components/CallToActionBtn.svelte
+++ src/components/CallToActionBtn.svelte
@@ -2,6 +2,6 @@
   class="btn my-6 overflow-hidden relative w-52 bg-pink-400 text-white py-4 px-4 rounded-xl uppercase -- before:block before:absolute before:h-full before:w-full before:bg-pink-500 before:left-0 before:top-0 before:-translate-y-full hover:before:translate-y-0 before:transition-transform"
 >
   <span class="relative">
-    <a href="/">Agende uma sessão</a>
+    <a href="https://wa.me/5511947922772" target="_blank">Agende uma sessão</a>
   </span>
 </button>
--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,6 +1,20 @@
-<!-- component -->
-<link rel="stylesheet" href="/">
-<link rel="stylesheet" href="/">
+<script>
+  const sections = [
+    {name: 'Artigos',     link: '#artigos'},
+    {name: 'Beneficios',  link: '#beneficios'},
+    {name: 'Depoimentos', link: '#depoimnentos'},
+    {name: 'FAQ',         link: '#faq'},
+    {name: 'Servicos',    link: '#servicos'},
+    {name: 'Sobre',       link: '#sobre'},
+]
+
+const contactForms = [
+  {name: 'Email', link: 'mailto:psy.dayanebartolomeu@gmail.com', icon: "fa-regular fa-envelope-open"},
+  {name: 'Instagram', link: 'https://www.instagram.com/psi.dayanebartolomeu/', icon: "fab fa-instagram"},
+  {name: 'Whatsapp', link: 'https://wa.me/5511947922772', icon: "fa-brands fa-whatsapp"},
+
+]
+</script>
 
 <footer class="relative bg-pink-100 pt-8 pb-6">
   <div class="container mx-auto px-4">
@@ -11,42 +25,25 @@
           Você consegue me encontrar nas plataformas abaixo.
         </h5>
         <div class="mt-6 lg:mb-0 mb-6">
+          {#each contactForms as contact}
           <button class="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-            <i class="fab fa-twitter"></i></button><button class="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-            <i class="fab fa-facebook-square"></i></button><button class="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-            <i class="fab fa-dribbble"></i></button><button class="bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-            <i class="fab fa-github"></i>
+            <a href={contact.link} target="_blank"> 
+              <i class={contact.icon}></i>
+            </a>
           </button>
+         {/each}
         </div>
       </div>
       <div class="w-full lg:w-6/12 px-4">
         <div class="flex flex-wrap items-top mb-6">
           <div class="w-full lg:w-4/12 px-4 ml-auto">
-            <span class="block uppercase text-gray-500 text-sm font-semibold mb-2">Links úteis</span>
+            <span class="block uppercase text-gray-500 text-sm font-semibold mb-2">Navegue</span>
             <ul class="list-unstyled">
+              {#each sections as section}
               <li>
-                <a class="text-gray-400 hover:text-pink-500 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/presentation?ref=njs-profile">Sobre</a>
+                <a class="text-gray-400 hover:text-pink-500 font-semibold block pb-2 text-sm" href={section.link}>{section.name}</a>
               </li>
-              <li>
-                <a class="text-gray-400 hover:text-pink-500 font-semibold block pb-2 text-sm" href="https://blog.creative-tim.com?ref=njs-profile">Benefícios</a>
-              </li>
-              <li>
-                <a class="text-gray-400 hover:text-pink-500 font-semibold block pb-2 text-sm" href="https://www.github.com/creativetimofficial?ref=njs-profile">Artigos</a>
-              </li>
-              <li>
-                <a class="text-gray-400 hover:text-pink-500 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile">Serviços</a>
-              </li>
-            </ul>
-          </div>
-          <div class="w-full lg:w-4/12 px-4">
-            <span class="block uppercase text-gray-500 text-sm font-semibold mb-2">OUTROS</span>
-            <ul class="list-unstyled">
-              <li>
-                <a class="text-gray-400 hover:text-pink-500 font-semibold block pb-2 text-sm" href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-profile">Depoimentos</a>
-              </li>
-              <li>
-                <a class="text-gray-400 hover:text-pink-500 font-semibold block pb-2 text-sm" href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-profile">FAQ</a>
-              </li>
+             {/each}
             </ul>
           </div>
         </div>
